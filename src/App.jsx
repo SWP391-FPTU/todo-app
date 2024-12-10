c40a99c1cf60a5bf67448ca22d07c3317a5a9709
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-12-10 21:43:25 +0700

    fix: header content

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-5 bg-blue-200 p-10">
       <div className="h-full w-full max-w-screen-md rounded-xl border bg-white p-10 shadow">
-        <h1 className="mb-5 text-2xl font-medium">Doing something!</h1>
+        <h1 className="mb-5 text-2xl font-medium">Doing something good!</h1>
         <Form addTask={addTask} />
         <TaskList tasks={tasks} />
       </div>
