69231af3f6cca7da32eb07da69d2aef2491f111c
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-12-10 19:49:47 +0700

    chore: init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex h-screen items-center justify-center p-10">
-      <h1 className="text-center text-4xl font-bold">Hello, World!</h1>
+      <h1 className="text-center text-4xl font-bold">Hello, TVK!</h1>
     </div>
   );
 }
