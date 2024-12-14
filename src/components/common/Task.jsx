913b423244ff863de3d0851af4ee54d20e488032
Nguyen Swan <149929360+NguyenSwan@users.noreply.github.com>
2024-12-14 16:55:28 +0700

    feat: add and delete task

--- src/components/common/Task.jsx
+++ src/components/common/Task.jsx
@@ -1,10 +1,25 @@
 import PropTypes from 'prop-types';
 
-const Task = ({ title }) => {
+// const Task = ({ title }) => {
+//   return (
+//     <div className="rounded-sm border px-5 py-2">
+//       <h3 className="text-base font-medium">{title}</h3>
+//       <button
+//       >Xóa</button>
+//     </div>
+//   );
+// };
+
+const Task = ({ title, onDelete }) => {
   return (
-    <div className="rounded-sm border px-5 py-2">
+    <div className="flex items-center justify-between rounded-sm border px-5 py-2">
       <h3 className="text-base font-medium">{title}</h3>
-      <button>Xóa</button>
+      <button
+        onClick={onDelete}
+        className="rounded bg-red-500 px-4 py-2 text-sm text-white hover:bg-red-600"
+      >
+        Xóa
+      </button>
     </div>
   );
 };
--- src/components/common/TaskList.jsx
+++ src/components/common/TaskList.jsx
@@ -1,14 +1,31 @@
 import Task from './Task';
 import PropTypes from 'prop-types';
 
-const TaskList = ({ tasks }) => {
+// const TaskList = ({ tasks }) => {
+//   if (tasks.length === 0)
+//     return <div className="mt-5 text-sm">Chưa có task nào!</div>;
+
+//   return (
+//     <div className="mt-5 space-y-2 text-sm">
+//       {tasks.map((t, index) => (
+//         <Task key={index} title={t.title} />
+//       ))}
+//     </div>
+//   );
+// };
+
+const TaskList = ({ tasks, deleteTask }) => {
   if (tasks.length === 0)
     return <div className="mt-5 text-sm">Chưa có task nào!</div>;
 
   return (
     <div className="mt-5 space-y-2 text-sm">
-      {tasks.map((t, index) => (
-        <Task key={index} title={t.title} />
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          title={task.title}
+          onDelete={() => deleteTask(task.id)} // Gọi hàm xóa khi nhấn nút "Xóa"
+        />
       ))}
     </div>
   );
